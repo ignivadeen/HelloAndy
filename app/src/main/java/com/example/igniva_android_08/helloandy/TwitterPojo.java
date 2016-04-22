e729956be7c249ac3384392974e3eac98e91c8cb
ignivajitender <jitender.github@ignivasolutions.com>
2016-04-22 13:04:48 +0530

    Update TwitterPojo.java
    updated to integer 1

--- app/src/main/java/com/example/igniva_android_08/helloandy/TwitterPojo.java
+++ app/src/main/java/com/example/igniva_android_08/helloandy/TwitterPojo.java
@@ -6,7 +6,7 @@ package com.example.igniva_android_08.helloandy;
  */
 public class TwitterPojo {
 
-    // pojo class
+    // pojo class 1
 
     private String name;
     private String handleName;
